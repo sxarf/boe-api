63bfce5059dd018c9885db529ae1ff941cc7e69e
sxarf <coinrizzlines@gmail.com>
2026-03-03 20:08:48 +0000

    Add SERVER_DATA object and update API_KEY definition

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 export interface Env {
   MY_BUCKET: R2Bucket;
   DB: D1Database;
-  API_KEY: string; 
+  API_KEY: string;
 }
+
+const SERVER_DATA = {
+  "min-console-version": "2.9.0",
+  "min-version": "1.0.6",
+  "menu-version": "1.0.7",
+  "admins": [
+    { "name": "cdev", "user-id": "CAF71E0462685D4A" },
+    { "name": "sxarf", "user-id": "E1DBFD3996EFOBBE" }
+  ],
+  "super-admins": [
+    "cdev",
+    "sxarf"
+  ],
+};
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
+
+    if (url.pathname === "/serverdata") {
+      return new Response(JSON.stringify(SERVER_DATA, null, 2), {
+        headers: { "Content-Type": "application/json" }
+      });
+    }
 
     const authHeader = request.headers.get("X-Custom-Auth");
     if (authHeader !== env.API_KEY) {
@@ -16,7 +36,6 @@ export default {
     if (url.pathname === "/download/loader") {
       const object = await env.MY_BUCKET.get('boe.onl.exe');
       if (!object) return new Response("file not found", { status: 404 });
-
       const headers = new Headers();
       object.writeHttpMetadata(headers);
       headers.set('Content-Type', 'application/octet-stream');
